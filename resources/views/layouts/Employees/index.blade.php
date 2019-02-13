d6642d9ecd01306a25691700e972cfb636aded46
glai <gjoyce94@gmail.com>
2019-02-13 11:34:53 +0800

    - Content initial pages blade files

---
+++ resources/views/layouts/Employees/index.blade.php
@@ -0,0 +1,50 @@
+<div class="row m-0 pt-2">
+  <div class="col-12" id="page-content">
+    <h1 class="display-4">Employees</h1>
+  </div>
+</div>
+
+<div class="row m-0 pt-2">
+  <div class="col-12" id="page-content">
+
+    {{-- table --}}
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Company</th>
+          <th scope="col">Department</th>
+          <th scope="col">Date Hired</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
---
+++ resources/views/layouts/Users/index.blade.php
@@ -0,0 +1,47 @@
+<div class="row m-0 pt-2">
+  <div class="col-12">
+    <h1 class="display-4">User Profile</h1>
+    <button type="button"
+            class="btn btn-outline-dark new-user"><i class="fas fa-user-plus"></i></button>
+  </div>
+</div>
+
+<div class="row m-0 pt-2">
+  <div class="col-12" id="page-content">
+
+    {{-- table --}}
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Username</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
