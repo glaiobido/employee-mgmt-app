5b3ed52f2a5a27382592d186d380994a22948e60
glai <gjoyce94@gmail.com>
2019-02-13 22:48:15 +0800

    User Table Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = new User;
+        $user->firstname = 'Test';
+        $user->lasttname = 'User';
+        $user->username = 'TestUser';
+        $user->email = 'testuser@test.com';
+        $user->password = Hash::make('123123');
+        $user->save();
     }
 }
